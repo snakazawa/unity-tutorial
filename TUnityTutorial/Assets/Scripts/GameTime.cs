5d3cf9479a6ee241c6b73728e986a08d5760aa11
snakazawa <nakazawa.tms0314@gmail.com>
2017-06-11 16:27:13 +0900

    end of TUnityTutorial

---
+++ TUnityTutorial/Assets/Scripts/GameTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTime : MonoBehaviour
+{
+
+	public Text text;
+	private float time;
+	
+
+	void Start ()
+	{
+		time = 0.0f;
+		text.text = time.ToString();
+	}
+	
+	void Update ()
+	{
+		time += Time.deltaTime;
+		text.text = time.ToString();
+	}
+}
